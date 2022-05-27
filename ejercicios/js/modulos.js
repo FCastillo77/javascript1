013fe000f1f4cff4277565849d6b9af64b4158bf
FCastillo77 <30987371+FCastillo77@users.noreply.github.com>
2022-05-27 19:05:54 -0400

    Add files via upload

---
+++ ejercicios/js/modulos.js
@@ -0,0 +1,12 @@
+import{calcularletras} from "./ejercicio1.js";
+import{palabrarecortada} from "./ejercicio2.js";
+import{separar} from "./ejercicio3.js";
+import {repetircad} from "./ejercicio4.js";
+console.log('Ejercicio 1');
+console.log(calcularletras("arbol"));
+console.log('Ejercicio 2');
+console.log(palabrarecortada("Hola mundo",7));
+console.log('Ejercicio 3');
+console.log(separar("Hola que-tal","-"));
+console.log('Ejercicio 4');
+repetircad("hola  mundo", 2);
